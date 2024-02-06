a6f1c9c85df6349e408940bf1f5ee8935954ef22
Hariharan <harimaniam@gmail.com>
2024-02-06 12:40:41 +0000

    HH-02-07: Work with django admin.

--- wisdompets/adoptions/models.py
+++ wisdompets/adoptions/models.py
@@ -12,6 +12,9 @@ class Pet(models.Model):
     age = models.IntegerField(null=True)
     vaccinations = models.ManyToManyField('Vaccine', blank=True)
 
-class Vaccine(models.Model) {
+class Vaccine(models.Model) :
     name = models.CharField(max_length=50)
-}
+
+    def __str__(self):
+        return self.name
+
